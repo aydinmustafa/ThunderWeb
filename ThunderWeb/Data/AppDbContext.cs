9394273f5d8b92e05545c5c2b2cb9d226f284094
Mustafa.Aydin <Mustafa.Aydin@STJMUSTAFACAN>
2020-07-29 19:19:50 +0300

    all of the code

---
+++ ThunderWeb/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿//using Microsoft.EntityFrameworkCore;
+//using Microsoft.EntityFrameworkCore.Internal;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+
+//namespace ThunderWeb.Data
+//{
+//    public class AppDbContext : DbContext
+//    {
+//        public AppDbContext(string connectionString) : base(connectionString)
+//        {
+//            //BU SINIFIN TAMAMI YENİ OLUŞTURULDU
+//        }
+//        public InternalDbSet<Product> Products { get; set; }
+//    }
+//}
---
+++ ThunderWeb/Data/Product.cs
@@ -0,0 +1,13 @@
+﻿//using System;
+//using System.ComponentModel.DataAnnotations;
+
+//namespace ThunderWeb.Data
+//{
+//    //BU SINIFIN TAMAMI YENİ OLUŞTURULDU
+//    public class Product
+//    {
+//        [Key]
+//        public Guid ProductId { get; set; }
+//        public string name { get; set; }
+//    }
+//}
