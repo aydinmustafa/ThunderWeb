967d5300cf743964835123222ea4bda5b189f0d2
Mustafa.Aydin <Mustafa.Aydin@STJMUSTAFACAN>
2020-07-30 00:48:00 +0300

    all the last changes

--- Database2/dbo/Tables/book.sql
+++ Database2/dbo/Tables/book.sql
@@ -1,11 +1,7 @@
 ﻿CREATE TABLE [dbo].[book] (
     [id]   NVARCHAR (36) NOT NULL,
     [name] NVARCHAR (36) NULL,
-    [author] NVARCHAR(50) NULL, 
-    [reader] NVARCHAR(50) NULL, 
-    [comment] NVARCHAR(50) NULL, 
-    [point] INT NULL, 
-    [advice] NVARCHAR(50) NULL, 
+   
     PRIMARY KEY CLUSTERED ([id] ASC)
 );
 
